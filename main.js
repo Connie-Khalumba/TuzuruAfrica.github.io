0d2fb5cc7de6771cdc05c606df529aa6f4943d52
constance khalumba <constancekhalumba@gmail.com>
2024-06-26 08:15:30 +0300

    Updated commits

---
+++ main.js
@@ -0,0 +1,16 @@
+let currentIndex = 0;
+
+const slides = document.querySelectorAll('.slides img');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    const slider = document.querySelector('.slides');
+    slider.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 3000);
